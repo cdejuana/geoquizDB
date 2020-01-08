ac5d5f82c751220d0128205c29f54dafa1dc3bde
Carlos de Juana <carlosdejuanajimenez@gmail.com>
2020-01-08 13:21:46 +0100

    añadiendo archivos

---
+++ app/src/main/java/com/daw2/geoquiz/MainActivity.java
@@ -0,0 +1,128 @@
+package com.daw2.geoquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_textoPregunta;
+    private Button bt_opcionA;
+    private Button bt_opcionB;
+    private Button bt_opcionC;
+    private Button bt_siguiente;
+    private Button bt_ayuda;
+    private Button bt_pista;
+    private ImageView iv_imagen;
+    private int indice;
+    private ArrayList <Pregunta> listaPreguntas = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_textoPregunta = findViewById(R.id.tv_pregunta);
+        bt_opcionA = findViewById(R.id.bt_A);
+        bt_opcionB = findViewById(R.id.bt_B);
+        bt_opcionC = findViewById(R.id.bt_C);
+        bt_siguiente = findViewById(R.id.bt_siguiente);
+        iv_imagen = findViewById(R.id.iv_ciudad1);
+        bt_ayuda = findViewById(R.id.bt_ayuda);
+        bt_pista = findViewById(R.id.bt_pista);
+        indice = 0;
+
+        PreguntasDB pregdb = new PreguntasDB(getApplicationContext());
+        listaPreguntas = pregdb.cargarListaPreguntas();
+
+
+        //ESTO YA NO HACE FALTA PORQUE COGEMOS LA LISTA DE PREGUNTAS DE LA BBDD:
+        /*Pregunta preg1 = new Pregunta(R.drawable.madrid, R.string.pregunta1, R.string.p1_madrid, R.string.p1_londres, R.string.p1_paris, 1, R.string.botonPista);
+        Pregunta preg2 = new Pregunta(R.drawable.tamesis, R.string.pregunta2, R.string.p2_manzanares, R.string.p2_sena, R.string.p2_tamesis, 2, R.string.botonPista);
+        Pregunta preg3 = new Pregunta(R.drawable.kilimanjaro, R.string.pregunta3, R.string.p3_everest, R.string.p3_kilimanjaro, R.string.p3_teide, 3, R.string.botonPista);
+
+        listaPreguntas.add(preg1);
+        listaPreguntas.add(preg2);
+        listaPreguntas.add(preg3);*/
+
+        bt_opcionA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (listaPreguntas.get(indice).getResultado() == 1){
+                    Toast.makeText(MainActivity.this, R.string.acierto, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, R.string.fallo, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        bt_opcionB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (listaPreguntas.get(indice).getResultado() == 3){
+                    Toast.makeText(MainActivity.this, R.string.acierto, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, R.string.fallo, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        bt_opcionC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (listaPreguntas.get(indice).getResultado() == 2){
+                    Toast.makeText(MainActivity.this, R.string.acierto, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, R.string.fallo, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        bt_siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (indice < (listaPreguntas.size()-1)){
+                    indice ++;
+                    cambiarPregunta();
+                } else {
+                    Toast.makeText(MainActivity.this, R.string.ultima_pregunta, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        bt_ayuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, AyudaActivity.class);
+                startActivity(i);
+            }
+        });
+
+        bt_pista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), PistaActivity.class);
+                i.putExtra("PISTA", listaPreguntas.get(indice).getPista());
+                startActivity(i);
+            }
+        });
+    }
+
+    private void cambiarPregunta() {
+        tv_textoPregunta.setText(listaPreguntas.get(indice).getTexto_pregunta());
+        iv_imagen.setImageResource(listaPreguntas.get(indice).getImagen());
+        bt_opcionA.setText(listaPreguntas.get(indice).getOpcion1());
+        bt_opcionB.setText(listaPreguntas.get(indice).getOpcion2());
+        bt_opcionC.setText(listaPreguntas.get(indice).getOpcion3());
+    }
+}
---
+++ app/src/main/java/com/daw2/geoquiz/PreguntasDB.java
@@ -0,0 +1,102 @@
+package com.daw2.geoquiz;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class PreguntasDB extends SQLiteOpenHelper {
+
+    //creamos contexto, necesario para
+    private Context context;
+
+    //creamos bbdd
+    public PreguntasDB(Context context){
+        super(context, "PreguntasBD", null, 1);
+        this.context = context;
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sqlCreate = "CREATE TABLE preguntas (" +
+                "idPregunta INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "imagen TEXT, " +
+                "textoPregunta TEXT, " +
+                "opcionA TEXT, " +
+                "opcionB TEXT, " +
+                "opcionC TEXT, " +
+                "respuesta INTEGER, " +
+                "pista TEXT" + ")";
+        db.execSQL(sqlCreate);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    public void instertarPregunta(Pregunta pregunta){
+        // ponemos bbd paraescritura
+        SQLiteDatabase db = getWritableDatabase();
+        //
+        String SQLInsert = "INSERT INTO preguntas (imagen, textoPregunta, opcionA, opcionB, opcionC," +
+                "respuesta, pista) VALUES ("+
+                "'" + pregunta.getImagen() +
+                "', '" + pregunta.getTexto_pregunta() +
+                "', '" + pregunta.getOpcion1() +
+                "', '" + pregunta.getOpcion2() +
+                "', '" + pregunta.getOpcion3() +
+                "', " + pregunta.getResultado() +
+                ", '" + pregunta.getPista() + "';";
+        db.execSQL(SQLInsert);
+        db.close();
+    }
+
+    public void instertarListaPreguntas(ArrayList<Pregunta> listaPreguntas){
+        SQLiteDatabase db = getWritableDatabase();
+        for (int i = 0; i < listaPreguntas.size(); i++) {
+            Pregunta pregunta = listaPreguntas.get(i);
+            String SQLInsert =
+                    "INSERT INTO pregunta (imagen, textoPregunta, opcionA, opcionB, opcionC," +
+                    "respuesta, pista)" +
+                    "VALUES (" +
+                    "'" + pregunta.getImagen() +
+                    "', '" + pregunta.getTexto_pregunta() +
+                    "', '" + pregunta.getOpcion1() +
+                    "', '" + pregunta.getOpcion2() +
+                    "', '" + pregunta.getOpcion3() +
+                    "', " + pregunta.getResultado() +
+                    ", '" + pregunta.getPista() + "';";
+
+            db.execSQL(SQLInsert);
+        }
+        db.close();
+    }
+
+    public ArrayList<Pregunta> cargarListaPreguntas(){
+        ArrayList<Pregunta> listaPreguntas = new ArrayList<>();
+        String[] CAMPOS = {"idPregunta", "imagen", "textoPregunta",
+                            "opcionA", "opcionB", "opcionC", "respuesta", "pista"};
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.query("preguntas", CAMPOS, null,
+                                null, null, null, null);
+        while(cursor.moveToNext()){
+            Pregunta pregunta = new Pregunta(cursor.getInt(0),
+                    cursor.getString(1),
+                    cursor.getString(2),
+                    cursor.getString(3),
+                    cursor.getString(4),
+                    cursor.getString(5),
+                    cursor.getInt(6),
+                    cursor.getString(7));
+            listaPreguntas.add(pregunta);
+        }
+        cursor.close();
+        db.close();
+        return listaPreguntas;
+    }
+
+}
